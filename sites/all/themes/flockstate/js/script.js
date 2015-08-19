163bd3da2164244a35ce39cdb5e6557692694e71
Martin Oru <martinoru@gmail.com>
2015-08-19 14:59:37 +0100

    jQuery to capitalize as text is inputed

--- sites/all/themes/flockstate/js/script.js
+++ sites/all/themes/flockstate/js/script.js
@@ -238,6 +238,14 @@ jQuery('.addmail').attr('title', 'Add Email')
                 jQuery('table').trigger('footable_filter', {filter: searchterm});
         });
 
+   $('input#edit-field-surname-und-0-value').add('input#edit-field-name-und-0-value').keyup(function(event) {
+            var textBox = event.target;
+            var start = textBox.selectionStart;
+            var end = textBox.selectionEnd;
+            textBox.value = textBox.value.charAt(0).toUpperCase() + textBox.value.slice(1);
+            textBox.setSelectionRange(start, end);
+        });
+
 
 
 
