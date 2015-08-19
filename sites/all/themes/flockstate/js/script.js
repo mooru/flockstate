83624bbe19963d1e61034554c51b7a22c64856a6
Martin Oru <martinoru@gmail.com>
2015-08-19 15:53:04 +0100

    removed duplicate jQuery capitalize script

--- sites/all/themes/flockstate/js/script.js
+++ sites/all/themes/flockstate/js/script.js
@@ -238,14 +238,7 @@ jQuery('.addmail').attr('title', 'Add Email')
                 jQuery('table').trigger('footable_filter', {filter: searchterm});
         });
 
-   // $('input#edit-field-surname-und-0-value').add('input#edit-field-name-und-0-value').keyup(function(event) {
-   //          var textBox = event.target;
-   //          var start = textBox.selectionStart;
-   //          var end = textBox.selectionEnd;
-   //          textBox.value = textBox.value.charAt(0).toUpperCase() + textBox.value.slice(1);
-   //          textBox.setSelectionRange(start, end);
-   //      });
-   //
+
    jQuery('input#edit-field-memail-und-0-email')
    .add('input#edit-field-memail-und-1-email')
    .Setcase({caseValue : 'lower'});
